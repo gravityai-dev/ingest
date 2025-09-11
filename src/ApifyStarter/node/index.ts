f02b8df5ad59b04ab0833fe45791e324991185d5
Gavin Payne <gavinapayne@gmail.com>
2025-09-11 15:52:56 +0400

    v1.0.10: version fix

--- src/ApifyStarter/node/index.ts
+++ src/ApifyStarter/node/index.ts
@@ -7,6 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
+    packageVersion: "1.0.9",
     type: NODE_TYPE,
     name: "Apify Starter",
     description: "Starts an Apify actor run with a list of URLs",
--- src/GoogleSheet/node/index.ts
+++ src/GoogleSheet/node/index.ts
@@ -7,6 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
+    packageVersion: "1.0.9",
     type: NODE_TYPE,
     category: "Ingest",
     name: "Google Sheet",
