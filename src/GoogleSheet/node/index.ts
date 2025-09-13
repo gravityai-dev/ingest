61aa7fc89376d9166a2832822ea1a85b50df55fd
Gavin Payne <gavinapayne@gmail.com>
2025-09-13 17:07:34 +0400

    v1.0.14

--- src/GoogleSheet/node/index.ts
+++ src/GoogleSheet/node/index.ts
@@ -7,7 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
 
   return {
-    packageVersion: "1.0.10",
+    packageVersion: "1.0.13",
     type: NODE_TYPE,
     category: "Ingest",
     name: "Google Sheet",
